8feae767992ad31c43c56db9f92187f9019bbfd3
Vitaly Kashavkin <kashavkin_v.v@mail.ru>
2026-02-18 13:31:41 +0500

    Начальная стадия проекта

--- complicatedСounter/ViewController.swift
+++ complicatedСounter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyValues: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    
+    @IBAction func plusButton(_ sender: Any) {
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+    }
+    
 }
 
