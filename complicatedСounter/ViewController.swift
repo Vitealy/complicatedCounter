ed0362312c19146c38c41da0823a7993743815ee
Vitaly Kashavkin <kashavkin_v.v@mail.ru>
2026-02-18 12:08:44 +0500

    Initial Commit

---
+++ complicatedСounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  complicatedСounter
+//
+//  Created by Vitaly Kashavkin on 18.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ complicatedСounterTests/complicated_ounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  complicated_ounterTests.swift
+//  complicatedСounterTests
+//
+//  Created by Vitaly Kashavkin on 18.02.2026.
+//
+
+import Testing
+@testable import complicated_ounter
+
+struct complicated_ounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
